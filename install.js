919b7e16a6a1dc93e3bb8a4529a64a58cfde73f9
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-27 14:26:35 +0200

    Correct file permission.

--- install.js
+++ install.js
@@ -1,6 +1,15 @@
+'use strict';
+var fs = require('fs');
 var downloadatomshell = require('gulp-download-atom-shell');
 
 downloadatomshell({
   version: '0.14.3',
   outputDir: 'atom'
-}, function(){});
+}, function(){
+
+  if (process.platform === 'linux'){
+    var atompath = require('./findpath');
+    fs.chmodSync(atompath, '0777');
+  }
+
+});
