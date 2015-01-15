aebb91730d0c7622f523844498d7d5f9bd257f81
Fabrice Weinberg <Fabrice@weinberg.me>
2015-01-15 20:08:49 +0100

    Add jquery

---
+++ atom-screenshot/preload.js
@@ -0,0 +1,4 @@
+// There is a problem loading jquery
+// due to it's popularity I am including it here
+// See https://github.com/atom/atom-shell/issues/254
+window.jQuery = window.$ = require('jquery');
