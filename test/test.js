05e0c6e595e777914150b57fc034ca8c7c56732d
Fabrice Weinberg <Fabrice@weinberg.me>
2015-01-22 00:37:10 +0100

    Use a small web server for testing

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 /* globals  assert, describe, it */
 'use strict';
 
+var http = require('http');
+
 var assert = require('assert');
 var imageSize = require('image-size');
 var isPng = require('is-png');
@@ -9,7 +11,6 @@ var screenshot = require('./../src');
 
 var timeout = 30000;
 
-var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
@@ -20,7 +21,7 @@ describe('screenshot', function(){
   it('should produce pngs', function(done){
     this.timeout(timeout);
 
-    screenshot({url : 'about:blank', width : 500, height : 500}).then(function(data) {
+    screenshot({url : 'http://127.0.0.1:9090', width : 500, height : 500}).then(function(data) {
       assert.ok(isPng(data));
       done();
     });
@@ -31,7 +32,7 @@ describe('screenshot', function(){
   it('should have a `delay` option', function(done) {
     this.timeout(timeout);
     var now = new Date();
-    screenshot({url : 'about:blank', delay : 2, width : 500, height : 500})
+    screenshot({url : 'http://127.0.0.1:9090', delay : 2, width : 500, height : 500})
     .then(function(){
       assert((new Date()) - now > 2000);
       done();
@@ -41,7 +42,7 @@ describe('screenshot', function(){
 
   it('should create a screenshot with 500x500 pixels', function(done){
     this.timeout(timeout);
-    screenshot({url : 'about:blank', width : 500, height : 500}).then(function(data){
+    screenshot({url : 'http://127.0.0.1:9090', width : 500, height : 500}).then(function(data){
       var size = imageSize(data);
       assert.equal(size.width, 500);
       assert.equal(size.height, 500);
@@ -53,7 +54,7 @@ describe('screenshot', function(){
   it('should warn about missing `height`', function(done) {
     this.timeout(timeout);
 
-    screenshot({url : 'about:blank', width : 500})
+    screenshot({url : 'http://127.0.0.1:9090', width : 500})
     .catch(function(err){
       assert.equal(err.message, 'At least `height` and `width` must be set');
       done();
