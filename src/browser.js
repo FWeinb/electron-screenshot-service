c3b74682552d67fa154cb74c5484ca11f19fd538
Fabrice Weinberg <Fabrice@weinberg.me>
2015-07-25 12:11:36 +0200

    Fix lint errors and tests

--- src/browser.js
+++ src/browser.js
@@ -7,18 +7,18 @@ var spawn = require('win-spawn');
 var Promise = require('bluebird');
 
 var electronpath = require('electron-prebuilt');
-var app =  path.join(__dirname, '../', 'electron-screenshot');
+var app = path.join(__dirname, '../', 'electron-service');
 
-var Browser = function( sock ) {
+var Browser = function (sock) {
   this.sock = sock;
 };
 
-Browser.prototype.screenshot = function( options ) {
+Browser.prototype.screenshot = function (options) {
   var deferred = Promise.pending();
 
-  options.delay = options.delay || 0;
+  options.delay = options.delay || 0;
 
-  if ( !options.width || !options.height) {
+  if (!options.width || !options.height) {
     deferred.reject(new Error('At least `height` and `width` must be set'));
     return deferred.promise;
   }
@@ -26,16 +26,16 @@ Browser.prototype.screenshot = function( options ) {
   if (options.crop) {
     if (!options.crop.x) { options.crop.x = 0; }
     if (!options.crop.y) { options.crop.y = 0; }
-    if (!options.crop.width || !options.crop.height) {
-     deferred.reject(new Error('In crop, at least `height` and `width` must be set'));
-     return deferred.promise;
+    if (!options.crop.width || !options.crop.height) {
+      deferred.reject(new Error('In crop, at least `height` and `width` must be set'));
+      return deferred.promise;
     }
   }
 
-  this.sock.send('take-screenshot', options, function(error, img){
+  this.sock.send('take-screenshot', options, function (error, img) {
     if (error) { deferred.reject(error); return; }
 
-    // Create a real buffer from data
+    // Make axon data a real buffer again
     img.data = new Buffer(img.data.data);
 
     deferred.resolve(img);
@@ -44,56 +44,59 @@ Browser.prototype.screenshot = function( options ) {
   return deferred.promise;
 };
 
-var _isStarted;
+var isStarted;
 var sock;
 var child;
 
-var createBrowser = function() {
+var createBrowser = function () {
   sock = axon.socket('req');
 
-  _isStarted = new Promise(function( resolve, reject ) {
+  isStarted = new Promise(function (resolve, reject) {
 
-    sock.on('connect', function() {
-        resolve( new Browser(sock) );
+    sock.on('connect', function () {
+      resolve(new Browser(sock));
     });
 
-    sock.on('error', function( error ) {
-        reject({ type: 'socket', error: error });
+    sock.on('error', function (error) {
+      reject({type: 'socket', error: error});
     });
 
     // Start the server on a free port
-    sock.bind(undefined, 'localhost', function() {
-        process.env.PORT = sock.server.address().port;
-        child = spawn(electronpath, [
-          '.'
-        ],{
-          cwd: app,
-          stdio: 'ignore'
-        });
+    sock.bind(undefined, 'localhost', function () {
+      process.env.PORT = sock.server.address().port;
+      child = spawn(electronpath, [
+        '.'
+      ],
+      {
+        cwd: app,
+        stdio: 'ignore'
+      });
 
-        child.on('exit', function(error){
-            reject({ type: 'electron', error: error });
-        });
+      child.on('exit', function (error) {
+        reject({type: 'electron', error: error});
+      });
     });
 
   });
 
-  return _isStarted;
+  return isStarted;
 };
 
 module.exports = {
 
-  getBrowser : function() {
-    return _isStarted || createBrowser();
+  getBrowser: function () {
+    return isStarted || createBrowser();
   },
 
-  close : function() {
-    _isStarted = undefined;
+  close: function () {
+    isStarted = undefined;
     if (sock) {
-      try{
+      try {
         sock.close();
         child.kill();
-      }catch(e){}
+      } catch(e) {
+        (function noop() {})();
+      }
     }
   }
 };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-/* globals  assert, describe, it */
 'use strict';
 
 var assert = require('assert');
@@ -6,47 +5,28 @@ var isPng = require('is-png');
 
 var screenshot = require('./../src');
 
-describe('screenshot', function(){
+describe('screenshot', function () {
   this.timeout(30000);
 
-  it('should produce pngs', function(done){
-    screenshot({url : 'about:blank', width : 500, height : 500}).then(function(img) {
+  it('should produce pngs', function (done) {
+    screenshot({url: 'about:blank', width: 500, height: 500})
+    .then(function (img) {
       assert.ok(isPng(img.data));
       done();
     });
   });
 
-
-  it('should have a `delay` option', function(done) {
-    var now = new Date();
-    screenshot({url : 'about:blank', delay : 2, width : 500, height : 500})
-    .then(function(){
-      assert((new Date()) - now > 2000);
-      done();
-    });
-  });
-
-
-  it('should create a screenshot with 500x500 pixels', function(done){
-    screenshot({url : 'about:blank', width : 500, height : 500}).then(function(img){
-      assert.equal(img.size.width, 500);
-      assert.equal(img.size.height, 500);
-      done();
-    });
-  });
-
-
-  it('should warn about missing `height`', function(done) {
-    screenshot({url : 'about:blank', width : 500})
-    .catch(function(err){
+  it('should warn about missing `height`', function (done) {
+    screenshot({url: 'about:blank', width: 500})
+    .catch(function (err) {
       assert.equal(err.message, 'At least `height` and `width` must be set');
       done();
     });
   });
 
-  it('should warn about missing `width`', function(done) {
-    screenshot({url : 'about:blank'})
-    .catch(function(err){
+  it('should warn about missing `width`', function (done) {
+    screenshot({url: 'about:blank'})
+    .catch(function (err) {
       assert.equal(err.message, 'At least `height` and `width` must be set');
       done();
     });
